69e0a76af774d61e919e55bb287bae6478f01c73
aakashshameer <aakashshameer@gmail.com>
2021-11-22 00:52:41 -0800

    adjust files

--- aggtable.R
+++ aggtable.R
@@ -1,11 +1,19 @@
 library(dplyr)
 library(readr)
+library(tidyverse)
+
+#Data from our athlete_events.csv
 athlete_events_data <- read.csv("src/data/olympic_history/athlete_events.csv")
 
+#Aggregate Table from our data from our athlete_events.csv
 aggregate_athletes_data <- athlete_events_data %>%
-  select(Sex, Age, Height, Weight, Team, Games, Sport, Medal)
+  select(Sex, Age, Height, Weight, Team, Games, Sport, Medal) 
 
 gdp_data <- read.csv("src/data/GDP_data.csv")
 
 aggregate_gdp_data <- gdp_data %>%
   select(Country.Name, Country.Code, X2016)
+
+aggregate_gdp_data <- aggregate_gdp_data %>%
+  rename("2016" = X2016) 
+  
--- summary_info.R
+++ summary_info.R
@@ -1,8 +1,12 @@
-install.packages("dplyr")
 library(dplyr)
-install.packages("readr")
 library(readr)
-read.csv("src/data/olympic_history/athlete_events.csv")
+library(stringr)
+athlete_events <- read.csv("src/data/olympic_history/athlete_events.csv")
+tokyo_olymic_medal <- read.csv(
+  "https://raw.githubusercontent.com/aakashshameer/info201-finalproject/main/src/data/olympic_data/Tokyo%20Medals%202021.csv")
+
+olympi_medal <- read.csv(
+  "https://raw.githubusercontent.com/aakashshameer/info201-finalproject/main/src/data/Summer_olympic_Medals.csv")
 
 
 # Average weight of men gold medalists for Swimming in 2016
@@ -180,6 +184,88 @@ United_Kingdom_2016_GDP <- GDP_data %>%
   filter(`Country Name` == "United Kingdom") %>%
   pull(`2016`)
 
+
+### Japan  Olympic  2020
+#China 
+jp_olympic_ch_medal <- olympi_medal %>%
+  filter(Host_country == "Japan") %>% 
+  filter(Year == 2020) %>%
+  filter(Country_Name == "China")
+#Japan
+jp_olympic_jp_medal <- olympi_medal %>%
+  filter(Host_country == "Japan") %>% 
+  filter(Year == 2020) %>%
+  filter(Country_Name == "Japan")
+#India
+jp_olympic_in_medal <- olympi_medal %>%
+  filter(Host_country == "Japan") %>% 
+  filter(Year == 2020) %>%
+  filter(Country_Name == "India")
+#United States
+jp_olympic_us_medal <- olympi_medal %>%
+  filter(Host_country == "Japan") %>% 
+  filter(Year == 2020) %>%
+  filter(Country_Name == "United States")
+#Germany
+jp_olympic_ge_medal <- olympi_medal %>%
+  filter(Host_country == "Japan") %>% 
+  filter(Year == 2020) %>%
+  filter(Country_Name == "Germany")
+#Great Britain
+jp_olympic_gb_medal <- olympi_medal %>%
+  filter(Host_country == "Japan") %>% 
+  filter(Year == 2020) %>%
+  filter(Country_Name == "Great Britain")
+
+#Medal count of 6 selected country in Tokyo Olympic
+tokyo_olympic_selected_country_medal <- rbind(
+  jp_olympic_ch_medal,
+  jp_olympic_jp_medal,
+  jp_olympic_in_medal,
+  jp_olympic_us_medal,
+  jp_olympic_gb_medal, 
+  jp_olympic_ge_medal)
+
+
+### Brazil  Olympic 2016
+#China 
+br_olympic_ch_medal <- olympi_medal %>%
+  filter(Host_country == "Brazil") %>% 
+  filter(Country_Name == "China")
+#Japan
+br_olympic_jp_medal <- olympi_medal %>%
+  filter(Host_country == "Brazil") %>% 
+  filter(Country_Name == "Japan")
+#India
+br_olympic_in_medal <- olympi_medal %>%
+  filter(Host_country == "Brazil") %>% 
+  filter(Country_Name == "India")
+#United States
+br_olympic_us_medal <- olympi_medal %>%
+  filter(Host_country == "Brazil") %>% 
+  filter(Country_Name == "United States")
+#Germany
+br_olympic_ge_medal <- olympi_medal %>%
+  filter(Host_country == "Brazil") %>% 
+  filter(Country_Name == "Germany")
+#Great Britain
+br_olympic_gb_medal <- olympi_medal %>%
+  filter(Host_country == "Brazil") %>% 
+  filter(Country_Name == "Great Britain")
+
+#medal count of 6 selected country in Brazil Olympics
+brazil_olympic_selected_country_medal <- rbind(
+  br_olympic_ch_medal,
+  br_olympic_jp_medal,
+  br_olympic_in_medal,
+  br_olympic_us_medal,
+  br_olympic_gb_medal, 
+  br_olympic_ge_medal)
+
+
+left_join(brazil_olympic_selected_country_medal, 
+          tokyo_olympic_selected_country_medal, 
+)
 
 
 
