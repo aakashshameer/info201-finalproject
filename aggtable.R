ffdfc70059fc680eeefaa1791e4f1976051a7c77
ewang07-2037338 <ewang07@uw.edu>
2021-11-22 15:40:33 -0800

    fixed agg table

--- aggtable.R
+++ aggtable.R
@@ -3,19 +3,20 @@ library("readr")
 
 #Olympic History: Athlete Events Data
 
-athlete_events_data <- read.csv("https://raw.githubusercontent.com/aakashshameer/info201-finalproject/main/src/data/olympic_history/athlete_events.csv")
+athlete_events_data <- read.csv("src/data/olympic_history/athlete_events.csv")
 
 aggregate_athletes_data <- athlete_events_data %>%
   select(Sex, Age, Height, Weight, Team, Games, Sport, Medal)
 
 #GDP Data
 
-gdp_data <- read.csv("https://raw.githubusercontent.com/aakashshameer/info201-finalproject/main/src/data/GDP_data.csv")
+gdp_data <- read.csv("src/data/GDP_data.csv", fileEncoding="UTF-8-BOM")
 
 aggregate_gdp_data <- gdp_data %>%
-  select(ï..Country.Name, Country.Code, X2016)
+  select(Country.Name, Country.Code, X2016)
 
-names(aggregate_gdp_data)[1] <- "Country Name"
+aggregate_gdp_data <- aggregate_gdp_data %>%
+  rename("Country Name" = Country.Name)
 
 aggregate_gdp_data <- aggregate_gdp_data %>%
   rename("Country Code" = Country.Code)
@@ -25,7 +26,7 @@ aggregate_gdp_data <- aggregate_gdp_data %>%
 
 #COVID-19 Data 
 
-covid_summary_data <- read.csv("https://raw.githubusercontent.com/aakashshameer/info201-finalproject/main/src/data/COVID-19_data/worldometer_coronavirus_summary_data.csv")
+covid_summary_data <- read.csv("src/data/COVID-19_data/worldometer_coronavirus_summary_data.csv")
 
 aggregate_summary_data <- covid_summary_data %>%
   select(country, active_cases)
@@ -40,7 +41,7 @@ aggregate_summary_data <- aggregate_summary_data %>%
 
 #Medals vs GDP
 
-medals_data <- read.csv("https://raw.githubusercontent.com/aakashshameer/info201-finalproject/main/src/data/olympic_data/Medals.csv")
+medals_data <- read.csv("src/data/olympic_data/Medals.csv", fileEncoding="UTF-8-BOM")
 
 names(medals_data)[1] <- "Rank"
 
@@ -52,10 +53,9 @@ medals_data <- medals_data %>%
 
 gdp_medals_data <- full_join(aggregate_gdp_data, medals_data) 
 
-
 #Tokyo Medals vs COVID Cases
 
-tokyo_medals_data <- read.csv("https://raw.githubusercontent.com/aakashshameer/info201-finalproject/main/src/data/olympic_data/Tokyo%20Medals%202021.csv")
+tokyo_medals_data <- read.csv("src/data/olympic_data/Tokyo Medals 2021.csv")
 
 tokyo_medals_data <- tokyo_medals_data %>%
   rename("Gold Medal" = Gold.Medal)
