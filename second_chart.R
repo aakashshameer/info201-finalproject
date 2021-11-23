73b4aad89cc4c9a160a8a2f53908fa56257618f0
shun20 <hosakashunsuke@MacBook-Air-2.local>
2021-11-22 17:48:16 -0800

    plot

---
+++ second_chart.R
@@ -0,0 +1,47 @@
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(tidyverse)
+library(hablar)
+library(ggrepel)
+
+source("aggtable.R")
+source("summary_info.R")
+
+#added total medal count in 2016 Brazil olympic medal count 
+brazil_total_medal <- brazil_olympic_selected_country_medal %>% 
+  mutate(total = Gold + Silver + Bronze) %>% 
+  rename("Country Name" = Country_Name)
+
+
+#Joined 2016 total medal counts of 6 country and their GDP
+brazil_oly_gdp_medal_count <- gdp_medals_data %>% 
+  left_join(brazil_total_medal, by = "Country Name") %>% 
+  select ("Country Name", "2016_GDP", "total")
+
+
+
+chart_gdp_medal_count <- ggplot(gdp_medals_data) +
+  geom_point(data = gdp_medals_data,
+             size = 3,
+             color = "#F285B8",
+             mapping = aes(x =  Total_Medals, y = GDP2016)) +
+  ggtitle("2016 GDP & Olympic Medal Count") +
+  theme(plot.title = element_text(size = 13, family = "mono", color = "#F285B8"), 
+        axis.text = element_text(color = "#525252", size = 8), 
+        text = element_text(size = 10, family = "mono"))+
+  labs(x = "Total Medal", y = "GDP") 
+  
+  
+
+
+
+  chart_two <- ggplot(jeuv) +
+    geom_point(data = jeuv,
+               mapping = aes(x = female_juvenile_jail_pop, y = male_juvenile_jail_pop)) +
+    ggtitle("Comparison of Male and Female juvenile population") +
+    labs(x = "Female Juvenile Population", y = "Male Juvenile Population") 
+
+
+  
+  
