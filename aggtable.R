28831a3b39be7d7f67a5b3cc09ecc9823e08ad48
ewang07-2037338 <ewang07@uw.edu>
2021-12-08 22:46:03 -0800

    aggtable round

--- aggtable.R
+++ aggtable.R
@@ -8,7 +8,9 @@ athlete_events_data <- read.csv("src/data/olympic_history/athlete_events.csv")
 
 aggregate_athletes_data <- athlete_events_data %>%
   select(Sex, Age, Height, Weight, Team, Games, Sport, Medal) %>%
-  group_by(Team)
+  group_by(Team) 
+
+aggregate_athletes_data$Weight <- sub("0+$", "", as.character(aggregate_athletes_data$Weight))
 
 #GDP Data
 
@@ -16,19 +18,10 @@ gdp_data2 <- read.csv("src/data/GDP_data.csv", fileEncoding="UTF-8-BOM")
 
 aggregate_gdp_data <- gdp_data2 %>%
   select(Country.Name, Country.Code, X2016) %>%
-  rename("Country_Name" = Country.Name) %>%
-  rename("Country Code" = Country.Code) %>%
-  rename("GDP2016" = X2016) 
-
-
-mutate_if(is.numeric, format(), 3)
-
-aggregate_gdp_data2 <- gdp_data2 %>%
-  select(Country.Name, Country.Code, X2016) %>%
   rename("Country Name" = Country.Name) %>%
   rename("Country Code" = Country.Code) %>%
-  rename("GDP2016" = X2016) %>% 
-  mutate_if(is.numeric, signif(digits = 3))
+  rename("GDP2016" = X2016) %>%
+  format(scientific = FALSE)
 
 #COVID-19 Data 
 
@@ -52,11 +45,12 @@ aggregate_2016_medal_data <- brazil2016_total_medal %>%
   select(Year, Host_country, Host_city, Country_Name, total)  %>%
   rename("Host Country" = Host_country) %>%
   rename("Host City" = Host_city) %>%
-  rename("Total_Medals" = total) 
+  rename("Total Medals" = total) %>%
+  rename("Country Name" = Country_Name)
 
 gdp_medals_data <- inner_join(aggregate_gdp_data, aggregate_2016_medal_data)
 
-gdp_medals_data$Total_Medals <- as.numeric(gdp_medals_data$Total_Medals)   
+gdp_medals_data$`Total Medals` <- as.numeric(gdp_medals_data$`Total Medals` )   
 
 #Tokyo Medals vs COVID Cases
 
@@ -66,6 +60,10 @@ tokyo_medals_data <- tokyo_medals_data %>%
   rename("Gold Medal" = Gold.Medal) %>%
   rename("Silver Medal" = Silver.Medal)  %>%
   rename("Bronze Medal" = Bronze.Medal) %>%
-  rename("Rank by Total" = Rank.By.Total)
+  rename("Rank by Total" = Rank.By.Total) %>%
+  arrange(`Rank by Total`) 
 
 tokyo_medals_covid_data <- full_join(tokyo_medals_data, aggregate_summary_data) 
+
+tokyo_medals_covid_data <- tokyo_medals_covid_data %>%
+  arrange(`Rank by Total`) 
